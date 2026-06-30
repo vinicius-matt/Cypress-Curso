024c4b9e46b463cb423856672350d316df40ca32
Matheus Miranda <matheusvini754@gmail.com>
2026-06-30 20:24:48 -0300

    praticando cypress

--- cypress/e2e/aula7.cy.js
+++ cypress/e2e/aula7.cy.js
@@ -7,4 +7,22 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
     cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
   })
+
+  //Trabalhando com pagina que abrem em outra aba
+  it('Trabalhando com pagina que abrem em outra aba', () => {
+  cy.contains('a','Política de Privacidade')
+   .should('have.attr','href','privacy.html')
+   .and('have.attr','target','_blank')
+})
+  
+//Acessa a pagina e remove atributo utilizando invoke
+ it('Acessa a pagina e remove atributo',() =>{
+    cy.contains('a','Política de Privacidade')
+    .invoke('removeAttr','target')
+    .click()
+
+    cy.contains('h1','CAC TAT - Política de Privacidade')
+    .should('be.visible')
+ })
+
 })
---
+++ cypress/e2e/privacyPolice.cy.js
@@ -0,0 +1,4 @@
+ //trabalhando com links que abrem em outra aba
+ it.only('Validando pagina de politica de privacidade de forma independente', () =>{
+    cy.visit('./src/privacy.html')
+ })
