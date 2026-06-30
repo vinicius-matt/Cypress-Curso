474faece53ce7e78c8b39b2e1b30881f5ac43bdc
Matheus Miranda <matheusvini754@gmail.com>
2026-06-30 19:19:47 -0300

    praticando alias

--- cypress/e2e/lesson6.cy.js
+++ cypress/e2e/lesson6.cy.js
@@ -13,7 +13,26 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#file-upload')
     .selectFile('cypress/fixtures/example.json')
     .should(input => {
-       expect(input[0].files[0].name).to.equal('example.json')// verificação de resultado
+    expect(input[0].files[0].name).to.equal('example.json')// verificação de resultado
     })
   })
+
+    //seleciona o arquivo simulando drag-and-drop como se fosse um usuario
+  it('seleciona o arquivo simulando drag-and-drop',() => {
+    cy.get('#file-upload')
+    .selectFile('cypress/fixtures/example.json',{action : 'drag-drop'})
+    .should(input => {
+    expect(input[0].files[0].name).to.equal('example.json')// verificação de resultado
+    })
+ })
+
+  //Trabalhando com alias
+ it.only('Selecione um arquivo utilizando uma fixture que possui alias',() =>{
+    cy.fixture('example.json').as('sampleFile')
+    cy.get('#file-upload')
+    .selectFile('@sampleFile')
+    .should(input => {
+    expect(input[0].files[0].name).to.equal('example.json')// verificação de resultado
+  })
+ })
 })
