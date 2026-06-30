65d3b6277ffd69d38a597cc16610660b3f940320
Matheus Miranda <matheusvini754@gmail.com>
2026-06-30 20:26:56 -0300

    praticando cypress metodo invoke

--- cypress/e2e/privacyPolice.cy.js
+++ cypress/e2e/privacyPolice.cy.js
@@ -1,4 +1,7 @@
  //trabalhando com links que abrem em outra aba
  it.only('Validando pagina de politica de privacidade de forma independente', () =>{
     cy.visit('./src/privacy.html')
+
+    cy.contains('h1','CAC TAT - Política de Privacidade')
+    .should('be.visible')
  })
